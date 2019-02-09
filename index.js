1d3ade0fd7971bf4b66b3a0891aee4e81d08e848
Antonio45 <sofia.gonzalez.maya@gmail.com>
2019-02-08 20:52:42 -0600

    calculadora operaciones

---
+++ index.js
@@ -0,0 +1,11 @@
+const operaciones = require('./math');
+
+// operaciones.suma(10, 20);
+// operaciones.resta(10, 20);
+// operaciones.division(10, 20);
+// operaciones.multiplicacion(10, 20);
+
+console.log(operaciones.suma(10, 20));
+console.log(operaciones.resta(10, 20));
+console.log(operaciones.division(10, 20));
+console.log(operaciones.multiplicacion(10, 20));
---
+++ math.js
@@ -0,0 +1,15 @@
+module.exports = {
+  suma:function (a, b) {
+    return a+b;
+  },
+  resta:function (a, b){
+    return a-b;
+  },
+  division:function (a, b){
+    return a/b;
+  },
+  multiplicacion:function (a, b){
+    return a*b;
+  }
+
+}
